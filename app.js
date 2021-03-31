29939f955b4b9339d170a34deae1439391578124
woosung lee <sungs3436@naver.com>
2021-03-31 15:59:56 +0900

    깃 저장소 초기화

---
+++ app.js
@@ -0,0 +1,22 @@
+//node는 v8엔진으로(자바스크립트 컴파일러) 코드를 해석해달라는것
+//http는 루트찾는걸 못해서 if문 그리고 한글처리도 따로 해줘야함
+
+// 모듈
+const express = require("express");
+const app = express();
+
+//라우팅
+const home = require("./routes/home");
+
+// 앱 세팅
+app.set("views", "./views");
+app.set("view engine", "ejs"); //ejs는 html이라고 생각해도 무관
+
+//router 받아오기
+//use는 미들웨어를 등록해주는 메서드 --
+app.use("/", home)  // routes/home로 가서 index.js내에 있는 render함수 실행
+
+
+module.exports = app;
+
+
